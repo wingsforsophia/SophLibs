b02c47fd0cd82623933ab3e6a7dc1d95f572a77a
Sophia Seremetis <shseremetis@gmail.com>
2020-11-17 17:09:40 -0500

    Adds value of input box into the madlib paragraph

--- js/app.js
+++ js/app.js
@@ -27,5 +27,5 @@ function myFunction() {
 
 
 
-printHere.innerHTML = `David Rush, who has broken more than 150 Guinness records to promote STEM education, said his recent experiences with mouth-catching records, including one for using his mouth to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
+printHere.innerHTML = `<p id="demo"></p>, who has broken more than 150 Guinness records to promote STEM education, said his recent experiences with mouth-catching records, including one for using his mouth to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
 
