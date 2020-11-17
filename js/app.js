c5d1305a952231f474f8acebc402e78b880a497a
Sophia Seremetis <shseremetis@gmail.com>
2020-11-17 15:02:17 -0500

    add form and print to screen

--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@
 const printHere = document.getElementById('print-here')
 
 
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 
@@ -17,4 +18,14 @@ const printHere = document.getElementById('print-here')
 /*-------------------------------- Functions --------------------------------*/
 
 
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("demo").innerHTML = x;
+  }
+
+
+
+
+
 printHere.innerHTML = `David Rush, who has broken more than 150 Guinness records to promote STEM education, said his recent experiences with mouth-catching records, including one for using his mouth to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
+
