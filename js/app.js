2c14d9756a8f1dcc16cb537206131af4a4d46529
Sophia Seremetis <shseremetis@gmail.com>
2020-11-17 21:44:25 -0500

    changes Print Here div

--- js/app.js
+++ js/app.js
@@ -35,5 +35,3 @@ printHere.innerHTML = `<div class="form form-disappear">
 <input type="text" id="myText" placeholder="Person's name">
 <button onclick="myFunction()">Enter</button>
 </div>   <p id="demo" class="input"></p>, who has broken more than 150 Guinness records to promote STEM education, said his recent experiences with mouth-catching records, including one for using his mouth to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
-
-
