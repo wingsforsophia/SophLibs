15f5dcf2795bae073f5b2f91d2393fc1a74a5aef
Sophia Seremetis <shseremetis@gmail.com>
2020-11-19 15:47:18 -0500

    make multiple forms disappear on enter

--- js/app.js
+++ js/app.js
@@ -9,11 +9,12 @@
 /*------------------------ Cached Element References ------------------------*/
 const printHere = document.getElementById('print-here')
 const getLibButton = document.getElementById('getLib')
-const forms = document.querySelector('.forms')
-
+const forms = document.querySelectorAll('.forms')
+const getNum = document.getElementById('getNum')
 /*----------------------------- Event Listeners -----------------------------*/
 
 getLibButton.addEventListener('click', getLib)
+
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -21,22 +22,19 @@ getLibButton.addEventListener('click', getLib)
 function  getLib() {
     let l1ProperNoun1 = document.getElementById("l1-properNoun1").value
 
+    let l1Number1 = document.getElementById("l1-number1").value
+   
+
+    forms.forEach(form => form = form.setAttribute("style", "display: none"))
 
 
-    
-    forms.setAttribute("style", "display: none")
     getLibButton.setAttribute("style", "display: none")
 
 
-    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than 10 Guinness records to promote STEM education, said his recent experiences with mouth-catching records, including one for using his mouth to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
+    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote STEM education, said his recent experiences with mouth-catching records, including one for using his mouth to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
 
   }
 
 
 
-  
-    // let l1Number1 = document.getElementById("l1-Number1").value
-    // console.log(l1Number1)
-
-
-
+console.log(forms)
