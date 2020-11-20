b9a9aeb47d04ce272be74a306f2ef4b5eb079ffa
Sophia Seremetis <shseremetis@gmail.com>
2020-11-20 11:49:05 -0500

    add third identifier option

--- js/app.js
+++ js/app.js
@@ -28,13 +28,14 @@ function  getLib1() {
     let l1identifier2 = document.getElementById('l1-identifier2').value
     let l1bodyPart2 = document.getElementById('l1-bodyPart2').value
     let l1sportball1 = document.getElementById('l1-sportball1').value
+    let l1identifier3 = document.getElementById('l1-identifier3').value
 
 
     forms.forEach(form => form = form.setAttribute("style", "display: none"))
     getLibButton.setAttribute("style", "display: none")
 
 
-    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote ${l1FourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a ${l1sportball1} off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
+    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote ${l1FourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a ${l1sportball1} off a wall, inspired ${l1identifier3} to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
 
   }
 
