8425344aada3f678dc31316e4c0602a813fe8c1c
Sophia Seremetis <shseremetis@gmail.com>
2020-11-20 11:42:46 -0500

    add value of sportball dropdown to story

--- js/app.js
+++ js/app.js
@@ -25,16 +25,16 @@ function  getLib1() {
     let l1FourLetterWord = document.getElementById('l1-fourLetterWord').value.toUpperCase()
     let l1identifier1 = document.getElementById('l1-identifier1').value
     let l1bodyPart1 = document.getElementById('l1-bodyPart1').value
-    let l1identifier2 = document.getElementById('l1-identifier1').value
+    let l1identifier2 = document.getElementById('l1-identifier2').value
     let l1bodyPart2 = document.getElementById('l1-bodyPart2').value
-    
+    let l1sportball1 = document.getElementById('l1-sportball1').value
 
 
     forms.forEach(form => form = form.setAttribute("style", "display: none"))
     getLibButton.setAttribute("style", "display: none")
 
 
-    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote ${l1FourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a table tennis ball off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
+    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote ${l1FourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a ${l1sportball1} off a wall, inspired him to try to set the record for most candy-coated corn caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
 
   }
 
