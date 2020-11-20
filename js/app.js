e3331c41efcb6237ef7f232d9a9182153b414400
Sophia Seremetis <shseremetis@gmail.com>
2020-11-20 11:55:54 -0500

    add value of body part 3 to the story

--- js/app.js
+++ js/app.js
@@ -26,12 +26,13 @@ function  getLib1() {
     let l1sportball1 = document.getElementById('l1-sportball1').value
     let l1identifier3 = document.getElementById('l1-identifier3').value
     let l1food1 = document.getElementById('l1-food1').value
+    let l1bodyPart3 = document.getElementById('l1-bodyPart3').value
 
     forms.forEach(form => form = form.setAttribute("style", "display: none"))
     getLibButton.setAttribute("style", "display: none")
 
 
-    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote ${l1FourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a ${l1sportball1} off a wall, inspired ${l1identifier3} to try to set the record for most ${l1food1} caught in the mouth by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
+    printHere.innerHTML = `${l1ProperNoun1}, who has broken more than ${l1Number1} Guinness records to promote ${l1FourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a ${l1sportball1} off a wall, inspired ${l1identifier3} to try to set the record for most ${l1food1} caught in the ${l1bodyPart3} by an individual in one minute (self-thrown). He caught 50 sweet little kernels. `
 
   }
 
