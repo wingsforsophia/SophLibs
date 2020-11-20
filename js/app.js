5b547d47fb8eb7a2033e9dedd6740dfa9525a3ad
Sophia Seremetis <shseremetis@gmail.com>
2020-11-20 12:36:04 -0500

    removes second body part choice

--- js/app.js
+++ js/app.js
@@ -22,7 +22,6 @@ function  getLib1() {
     let l1identifier1 = document.getElementById('l1-identifier1').value
     let l1bodyPart1 = document.getElementById('l1-bodyPart1').value.toLowerCase()
     let l1identifier2 = document.getElementById('l1-identifier2').value
-    let l1bodyPart2 = document.getElementById('l1-bodyPart2').value.toLowerCase()
     let l1sportball1 = document.getElementById('l1-sportball1').value
     let l1identifier3 = document.getElementById('l1-identifier3').value
     let l1food1 = document.getElementById('l1-food1').value.toLowerCase()
@@ -41,7 +40,7 @@ function  getLib1() {
     
 
 
-    printHere.innerHTML = `${capitalizeFirstLetter(l1properNoun1)}, who has broken more than ${l1number1} Guinness records to promote ${l1fourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart2} to bounce a ${l1sportball1} off a wall, inspired ${l1identifier3} to try to set the record for most ${l1food1} caught in the ${l1bodyPart3} by an individual in ${l1number2} minute(s), self-thrown.`
+    printHere.innerHTML = `${capitalizeFirstLetter(l1properNoun1)}, who has broken more than ${l1number1} Guinness records to promote ${l1fourLetterWord} education, said ${l1identifier1} recent experiences with ${l1bodyPart1}-catching records, including one for using ${l1identifier2} ${l1bodyPart1} to bounce a ${l1sportball1} off a wall, inspired ${l1identifier3} to try to set the record for most ${l1food1} caught in the ${l1bodyPart3} by an individual in ${l1number2} minute(s), self-thrown.`
 
   }
 
