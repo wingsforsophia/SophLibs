e04c70ee5c07f7d54c53e00648da9b422c0bbf78
Sophia Seremetis <shseremetis@gmail.com>
2020-11-22 21:03:51 -0500

    changes style l3

--- js/app.js
+++ js/app.js
@@ -187,7 +187,7 @@ function renderLib2() {
 // begin renderlib3()
 
 function renderLib3() {
-  let l3adjective1 = document.getElementById('l3-adjective1').value
+  let l3adjective1 = document.getElementById('l3-adjective1').value.toLowerCase()
   let l3thing1 = document.getElementById('l3-thing1').value.toLowerCase()
   let l3chore1 = document.getElementById('l3-chore1').value
   let l3name1 = document.getElementById('l3-name1').value
@@ -199,12 +199,7 @@ function renderLib3() {
 
   forms3.forEach(form => form = form.setAttribute("style", "display: none"))
 
-
-
-  function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
-  }
-
+  
   //makes each letter of string uppercase----------------------------------
   function uppercase(str) {
     let array1 = str.split(' ');
@@ -230,7 +225,7 @@ function renderLib3() {
  </div>`
 
 
-  l3printHere.innerHTML = `Thank you very much. This is a very ${l3adjective1} announcement. The biggest ever concerning ${l3thing1} and ${l3thing1} pricing. So today, I have to tell you it’s a great honor for me to announce that my administration is issuing two groundbreaking rules to very dramatically lower the price of prescription ${l3thing1} for the American people. Especially for our ${l3blueCheese1} seniors. We’re pleased to be joined on this occasion by Secretary of ${uppercase(l3chore1)}, ${capitalizeFirstLetter(l3name1)}, and Administrator of ${uppercase(l3dislike1)}, ${capitalizeFirstLetter(l3name2)}. Thank you both. Thank you both. And thank you all for being here.`
+  l3printHere.innerHTML = `Thank you very much. This is a very ${l3adjective1} announcement. The biggest ever concerning ${l3thing1} and ${l3thing1} pricing. So today, I have to tell you it’s a great honor for me to announce that my administration is issuing two groundbreaking rules to very dramatically lower the price of prescription ${l3thing1} for the American people. Especially for our ${l3blueCheese1} seniors. We’re pleased to be joined on this occasion by Secretary of ${uppercase(l3chore1)}, ${uppercase(l3name1)}, and Administrator of ${uppercase(l3dislike1)}, ${uppercase(l3name2)}. Thank you both. Thank you both. And thank you all for being here.`
 
 
 
